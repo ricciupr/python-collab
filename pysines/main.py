0200eb6e1cae4d18e50d34a53bd721ef411ff50d
ricciupr <ricciupr@mcmaster.ca>
2015-07-09 11:48:38 -0400

    I think we made a working program
    This should work

--- pysines/main.py
+++ pysines/main.py
@@ -16,7 +16,11 @@ def create_parser():
     Create the Argument Parser for this script
     :return:  An `argparse.ArgumentParser` instance
     """
-    parser = argparse.ArgumentParser('pysines', description="")
+    parser = argparse.ArgumentParser('pysines', description=" add description")
+    
+    parser.add_argument('-f', '--file',help="")
+    parser.add_argument('-n', '--num', type=int, help="")
+    
     return parser
 
 def parse_args(args):
@@ -26,17 +30,55 @@ def parse_args(args):
     :return: An `argparse.Namespace` object
     """
     parser = create_parser()
-    return parser.parse_args(args)
-
+    # runs the above funciton to create our parser object, this could have all been done here
+        
+    return parser.parse_args(args)  #parse_args() is a method of our parser object
+                                    # happens to have the same name as our function
+                                    # but they are not the same thing
+    
 def run(options):
     """
     Run the actual script
     :param options: An `argparse.Namespace` object
     :return:
     """
+    
+    from matplotlib import pyplot as plt
+    import numpy as np
+    import os
 
+    # select folder to save plots in                                     \/ go back one level
+    plot_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'plots'))  
+    
+    print options.file
+    print options.num
+    
+    
+    # Create the data set 
+    print
+    print "Plot 1"
+    time = np.linspace(0, 6 * np.pi)
+    data = 2 * np.sin(time) + 3 * np.cos(time)
+    
+    # Create the plot 
+    for i in range(1, options.num +1):
+        plt.plot(time, data*i)
+        
+    plt.title('A title')
+    plt.xlabel('Time')
+    plt.ylabel('Data')
+      
+    # Save the plot as an exaple
+    plt.savefig(os.path.join(plot_dir, options.file))
+      
+              
     # Do some additional check on the options
     check_options(options)
+    
+    
+    
+    
+    
 
     pass
 
@@ -44,5 +86,9 @@ def run(options):
 if __name__ == '__main__':
     # Script has been launched
 
-    options = parse_args(sys.argv)
+    parser = create_parser()
+    #options = parse_args(sys.argv)     #skip the parser_args() function and just do it below
+                                        # dont explicity reference the comand line 
+                                        #input sys.argv, it is already the default
+    options = parser.parse_args()      
     run(options)
